8cfae49651332f9c79bc4fc83191c7dd05a1a63d
Muhammed Nasser <mu7ammadnasser05@gmail.com>
2026-06-29 12:38:38 +0300

    EDIT

---
+++ src/routes/base.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends, FastAPI
+import os 
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags = ["/api/v1"]
+)
+@base_router.get("/")
+async def Welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    return {"message": f"Welcome to {app_name} v{app_version}!"}
